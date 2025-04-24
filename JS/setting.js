e7197b75e3cf823135b2fbd51b8b6dd115567b9c
Uno4n4 <sortlcrossvolee@gmail.com>
2025-04-24 14:48:52 +0200

    Ajout de div

--- JS/setting.js
+++ JS/setting.js
@@ -18,35 +18,65 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("reglage").onclick = function(){
         container.style.display = "block";
         container.innerHTML = 
-        `<form id="setting" class="form-style">
-        <h2>Modifier son profil</h2>
-        <label for="photoUpload">
-            <img src="../IMAGE/logo-iut.png" alt="Photo de profil" id="photo">
-        </label>
-        <input type="file" id="photoUpload" hidden>
-        <button type="button" id="changer">Changer</button>
-        <button type="button" id="supp">Supprimer la photo</button>
-        <br><label>Nom *</label>
-        <input type="text" id="Nom"><br>
-        <label>Prénom *</label>
-        <input type="text" id="Prenom"><br>
-        <label>Email *</label>
-        <input type="text" id="Email"><br>
-        <label>Numéro de téléphone *</label>
-        <input type="text" id="Tel"><br>
+        container.innerHTML = 
+`<form id="setting" class="form-style">
+  <h2>Modifier son profil</h2>
+  <div class="photo-container">
+    <label for="photoUpload">
+      <img src="../IMAGE/logo-iut.png" alt="Photo de profil" id="photo">
+    </label>
+    <input type="file" id="photoUpload" hidden>
+    <div class="button-container">
+      <button type="button" id="changer">Changer</button>
+      <button type="button" id="supp">Supprimer la photo</button>
+    </div>
+  </div>
+  <div class="form-grid">
+    <div>
+      <label>Nom *</label>
+      <input type="text" id="Nom">
+    </div>
+    <div>
+      <label>Prénom *</label>
+      <input type="text" id="Prenom">
+    </div>
+    <div>
+      <label>Email *</label>
+      <input type="text" id="Email">
+    </div>
+    <div>
+      <label>Numéro de téléphone *</label>
+      <input type="text" id="Tel">
+    </div>
+  </div>
+
+  <div class="button-container-1">
         <button type="submit" id="submit">Enregistrer les changements</button>
         <button onclick="../HTML/reglages.html" id="submit2">Annuler</button>
-        <hr>
-        <h2>Changer son mot de passe</h2>
-        <br><label>Ancien mot de passe</label>
-        <input type="password" id="old"><br>
-        <label>Nouveau mot de passe</label>
-        <input type="password" id="new"><br>
-        <label>Confirmer le mot de passe</label>
-        <input type="password" id="new1"><br>
+    </div>
+  <hr>
+  <h2>Changer son mot de passe</h2>
+  <div class="form-grid1">
+        <div>
+            <br><label>Ancien mot de passe</label>
+            <input type="password" id="old"><br>
+        </div>
+        <div>
+            <label>Nouveau mot de passe</label>
+            <input type="password" id="new"><br>
+        </div>
+        <div>
+            <label>Confirmer le mot de passe</label>
+            <input type="password" id="new1"><br>
+        </div>
+    </div>
+
+  <div class="button-container-2">
         <button type="submit" id="confirmer">Confirmer</button>
-        </form>
-        `;
+    </div>
+
+</form>`;
+
     };
 
     document.getElementById("verif").onclick = function(){
